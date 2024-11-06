0c4658efe90a919b7f87b3892cfc5e3f2a9c09f7
mat-star <mateus.silva.jesus@escola.pr.gov.br>
2024-11-06 08:40:06 -0300

    .

--- sript.js
+++ sript.js
@@ -1,23 +1,30 @@
 // Altera o texto do parágrafo inicial
 function alterarTexto() {
-}
 document.getElementById("text").innerText = "Ao clicar, eu mudei!!";
+}
+
 // Adiciona um novo parágrafo ao final do corpo
 function adicionarParagrafo() {
-const novoParagrafo = document.createElement("p");
-novoParagrafo.innerText = "Depois que você clicou, eu apareci !!"; document.body.appendChild(novoParagrafo);
+    const novoParagrafo = document.createElement("p");
+    novoParagrafo.innerText = "Depois que você clicou, eu apareci !!";
+     document.body.appendChild(novoParagrafo);
 }
 // Remove o último parágrafo, se houver mais de um
 function removerParagrafo() {
-}
-const paragrafos = document.querySelectorAll("p"); if (paragrafos.length > 1) {
+    const paragrafos = document.querySelectorAll("p"); 
+    if (paragrafos.length > 1) {
 paragrafos [paragrafos.length-1].remove();
-} else {
-}
-alert("Não há mais parágrafos para remover 11");
-// Muda a cor de fundo da página para uma cor aleatória function mudarCorFundo() {}
+} else { alert("Não há mais parágrafos para remover ");
 
-const corAleatoria = `#${Math.floor(Math.random() * 16777215).toString(16)}`; document.body.style.backgroundColor = corAleatoria;
+    }
+}
+// Muda a cor de fundo da página para uma cor aleatória 
+
+function mudarCorFundo() {
+const corAleatoria = `#${Math.floor(Math.random() * 16777215).toString(16)}`; 
+document.body.style.backgroundColor = corAleatoria;
+}
+
 // Muda a cor do texto do parágrafo inicial para uma cor aleatória
 function mudarCorTexto() {
 const cores = ["#FF5733", "#33FF57", "#3357FF", "#FF33A6", "#FFC133"];
